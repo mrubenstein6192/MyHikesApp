b1c5d87b417b13375ad1741040ebf8fd0430593c
Michael Rubenstein <mrubenstein6192@gmail.com>
2019-06-10 19:15:14 -0400

    again

--- routes/index.js
+++ routes/index.js
@@ -7,10 +7,10 @@ const path = require('path');
 
 router.use('/api', apiRoutes);
 
-if (process.env.NODE_ENV === 'production') {
-  router.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/build/index.html'))
-  });
-}
+// if (process.env.NODE_ENV === 'production') {
+//   router.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname, '../client/build/index.html'))
+//   });
+// }
 
 module.exports = router;
--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const routes = require('./routes');
 app.use(routes);
 
 if (process.env.NODE_ENV === "production") {
-  app.use('/static', express.static(path.join(__dirname, 'client/build')));
+  app.use(express.static("client/build"));
 }
 
 mongoose.connect(
